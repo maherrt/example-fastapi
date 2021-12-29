095f2da55e0d6fa5ce30cef07e5f558a1b7d0508
maherrt <maherrt@hotmail.com>
2021-12-29 19:18:43 +0000

    added Procfile

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -15,7 +15,7 @@ def vote(vote: schemas.Vote,
         
         post = db.query(models.Post).filter(models.Post.id == vote.post_id ).first()
         if not post:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post:{vote.post_id} does not exist")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post: {vote.post_id} does not exist")
 
         vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id )
         found_vote= vote_query.first()
